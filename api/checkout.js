7f1a73340694a7e9eb85d38b3657df9a06092cda
Leticia Piedade <77733535+pleticiapiedade@users.noreply.github.com>
2025-06-07 21:58:05 -0300

    Update checkout.js

--- api/checkout.js
+++ api/checkout.js
@@ -1,4 +1,12 @@
 export default async function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', 'https://vossavontade.com.br');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end(); // pré-flight OK
+      }
+      
     if (req.method !== 'POST') {
       return res.status(405).json({ error: 'Método não permitido' });
     }
